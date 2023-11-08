b4509e8831f76c1201ffca9a0ae6bee6e54de0d3
Elmeri Hakkinen <60952417+hakkd@users.noreply.github.com>
2023-11-07 20:37:47 -0800

    added post route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import pg from "postgres";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
@@ -26,7 +27,7 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
     try {
         const result = await db.query("SELECT * FROM reviews");
-        res.render("index.ejs");
+        res.render("index.ejs", {reviews: result.rows});
     } catch (err) {
         console.log(err);
     }
@@ -37,6 +38,22 @@ app.get("/", async (req, res) => {
 // - /edit route
 // - /delete route
 // - some kind of sorting function
+// use axios to get book covers from https://openlibrary.org/dev/docs/api/covers
+
+app.post("/add", async (req, res) => {
+    const title = req.body.title;
+    const review = req.body.review;
+    const rating = req.body.rating;
+    try {
+        await db.query(
+            "INSERT INTO reviews (title, date_created, review, rating) VALUES ($1, to_timestamp($2), $3, $4)",
+            [title, Date.now(), review, rating]
+        );
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 
 app.listen(port, () => {
