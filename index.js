780848e19390606095720c0b2a4de96b73e72da7
Elmeri Hakkinen <60952417+hakkd@users.noreply.github.com>
2023-11-14 16:14:41 -0800

    added view review and edit features

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get("/", async (req, res) => {
         let reviews = [];
         result.rows.forEach((row) => {
             let review = {
+                id: row.id,
                 title: row.title,
                 author: row.author,
                 review: row.review,
@@ -50,8 +51,6 @@ app.get("/new-review", (req, res) => {
 });
 
 // TODO:
-// - /edit route
-// - /delete route
 // - some kind of sorting function
 
 app.post("/add", async (req, res) => {
@@ -65,6 +64,62 @@ app.post("/add", async (req, res) => {
         await db.query(
             "INSERT INTO reviews (title, date_created, review, rating, isbn, author) VALUES ($1, to_timestamp($2), $3, $4, $5, $6)",
             [title, Date.now(), review, rating, isbn, author],
+        );
+        // TODO: add user to DB/link to review
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get("/view-review/:reviewId", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM reviews WHERE id = $1", [req.params.reviewId]);
+        let review = {
+            id: result.rows[0].id,
+            title: result.rows[0].title,
+            author: result.rows[0].author,
+            review: result.rows[0].review,
+            rating: result.rows[0].rating,
+            isbn: result.rows[0].isbn,
+            date: result.rows[0].date_created,
+        }
+        res.render("view-review.ejs", {review: review});
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get("/edit-review/:reviewId", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM reviews WHERE id = $1", [req.params.reviewId]);
+        let review = {
+            id: result.rows[0].id,
+            title: result.rows[0].title,
+            author: result.rows[0].author,
+            review: result.rows[0].review,
+            rating: result.rows[0].rating,
+            isbn: result.rows[0].isbn,
+            date: result.rows[0].date_created,
+        }
+        res.render("edit-review.ejs", {review: review});
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post("/edit-review/:reviewId", async (req, res) => {
+    const id = req.params.reviewId;
+    const title = req.body.title;
+    const author = req.body.author;
+    const review = req.body.review;
+    const rating = req.body.rating;
+    const isbn = req.body.isbn;
+    console.log(req.body);
+    try {
+        await db.query(
+            "UPDATE reviews SET title = $1, author = $2, review = $3, rating = $4, isbn = $5 WHERE id = $6",
+            [title, author, review, rating, isbn, id],
         );
         // TODO: add user to DB/link to review
         res.redirect("/");
